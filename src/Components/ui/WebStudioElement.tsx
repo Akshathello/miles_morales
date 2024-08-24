28e0f71ccc92348bdcd8621ec2c130d23ab2867a
Akshathello <akshatkhero@gmail.com>
2024-08-24 18:02:45 +0530

    14th commit

--- src/Components/ui/WebStudioElement.tsx
+++ src/Components/ui/WebStudioElement.tsx
@@ -1,6 +1,6 @@
 "use Client";
 import React, { useEffect, useRef, useState } from "react";
-import { motion } from "framer-motion";
+import { motion, useScroll } from "framer-motion";
 import mousePointer from "../utils/mousePointer";
 
 function WebStudioElement() {
@@ -30,9 +30,20 @@ function WebStudioElement() {
   //   };
   // }, [x, y]);
 
+  const element = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: element,
+    offset: ["start 0.9", "start 0.25"],
+  });
+
+  useEffect(() => {
+    scrollYProgress.on("change", (e) => console.log(e));
+  }, []);
+
   return (
     <div className=" h-[100rem] bg-[#030712]">
-      <div className=" px-[19rem] py-9 items-start -mb-9">
+      <div className=" px-[19rem] py-9 items-start -mb-9 pt-[10rem]">
         <div className="relative">
           <div className="absolute inset-0.5 rounded-lg bg-gradient-to-r from-orange-500 to bg-purple-600  blur-lg opacity-100 animate-pulse"></div>
 
@@ -41,18 +52,61 @@ function WebStudioElement() {
               Web Studio
             </span>
 
-            <span className="absolute text-gray-100 text-[10.8rem] whitespace-nowrap font-serif italic">
+            <motion.span
+              ref={element}
+              style={{ opacity: scrollYProgress }}
+              className="absolute text-gray-100 text-[10.8rem] whitespace-nowrap font-serif italic"
+            >
               Web Studio
-            </span>
+            </motion.span>
 
             <span className="absolute text-[10.8rem] whitespace-nowrap font-serif italic blur-sm">
               Web Studio
             </span>
 
-            <span className=" text-[#c65380] absolute text-[10.8rem] whitespace-nowrap font-serif italic blur-sm">
+            <motion.span
+              ref={element}
+              style={{ opacity: scrollYProgress }}
+              className="absolute text-[#ff6590] text-[10.8rem] whitespace-nowrap font-serif italic blur-sm"
+            >
               Web Studio
-            </span>
+            </motion.span>
           </div>
+        </div>
+      </div>
+      <div className=" flex justify-center items-center mt-20">
+        <div className=" flex leading-[65px] text-center">
+          <div className="font-serif text-white text-[4rem] blur-sm">
+            Godly Experience. The finer Level. The cosmic level. An experience
+            like none another. What it TAKES!. Passion Driven. Take the plunge.
+            Miles to Go!. Nonstop. Vision. Plan. <br /> Get Set Go!
+          </div>
+          <motion.div
+            animate={{
+              WebkitMaskPosition: `${x - size / 2}px ${y - size / 2}px`,
+              WebkitMaskSize: `${size}px`,
+            }}
+            transition={{ type: "tween", ease: "backOut" }}
+            style={{
+              WebkitMaskPosition: `${x - size / 2}px ${y - size / 2}px`,
+              WebkitMaskSize: `${size}px`,
+            }}
+            className="absolute leading-[65px] font-serif text-[4rem] mask"
+          >
+            <div
+              onMouseEnter={() => {
+                setIsHovered(true);
+              }}
+              onMouseLeave={() => {
+                setIsHovered(false);
+              }}
+              className=" bg-gradient-to-r from-orange-500 to bg-purple-600"
+            >
+              Godly Experience. The finer Level. The cosmic level. An experience
+              like none another. What it TAKES!. Passion Driven. Take the
+              plunge. Miles to Go!. Nonstop. Vision. Plan. <br /> Get Set Go!
+            </div>
+          </motion.div>
         </div>
       </div>
       <div className=" px-[14rem] py-16 items-center -mb-9">
@@ -110,41 +164,6 @@ function WebStudioElement() {
           experience designed between the lines of consumer experience.
         </div>
         <div className="font-serif font-extralight text-white"> </div>
-      </div>
-      <div className=" flex justify-center items-center mt-20">
-        <div className=" flex leading-[65px] text-center">
-          <div className="font-serif text-white text-[4rem] blur-sm">
-            Godly Experience. The finer Level. The cosmic level. An experience
-            like none another. What it TAKES!. Passion Driven. Take the plunge.
-            Miles to Go!. Nonstop. Vision. Plan. <br /> Get Set Go!
-          </div>
-          <motion.div
-            animate={{
-              WebkitMaskPosition: `${x - size / 2}px ${y - size / 2}px`,
-              WebkitMaskSize: `${size}px`,
-            }}
-            transition={{ type: "tween", ease: "backOut" }}
-            style={{
-              WebkitMaskPosition: `${x - size / 2}px ${y - size / 2}px`,
-              WebkitMaskSize: `${size}px`,
-            }}
-            className="absolute leading-[65px] font-serif text-[4rem] mask"
-          >
-            <div
-              onMouseEnter={() => {
-                setIsHovered(true);
-              }}
-              onMouseLeave={() => {
-                setIsHovered(false);
-              }}
-              className=" bg-gradient-to-r from-orange-500 to bg-purple-600"
-            >
-              Godly Experience. The finer Level. The cosmic level. An experience
-              like none another. What it TAKES!. Passion Driven. Take the
-              plunge. Miles to Go!. Nonstop. Vision. Plan. <br /> Get Set Go!
-            </div>
-          </motion.div>
-        </div>
       </div>
     </div>
   );
